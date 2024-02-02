41686c3b779a3461cc29a1ef2fa1a12435b7866b
Charlie-bennett98 <charleybennett698@gmail.com>
2024-02-02 21:28:13 +0000

    commiting for tom to have a look at code

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,86 +2,89 @@ import "./Form.scss";
 import axios from "axios";
 import aangInIce from "../../assets/images/aang-in-ice.jpg";
 import { useState } from "react";
-import cardValue from "../wind-card/Wind-Card";
+// import cardValue from "../wind-card/Wind-Card";
 import WindCard from "../wind-card/Wind-Card";
+import { render } from "@testing-library/react";
 
-const Form = () => {
+const Form = (props) => {
 
-const [cardValue, setCardValue] = useState("");
+  function IsWindSpeedHigh(windSpeed) {
+    if (windSpeed > 10) {
+      return `Wind speed seems to be ${windSpeed + "mph"}, maybe Aang was here`;
+    } else {
+      return `wind speed seems to be ${
+        windSpeed + "mph"
+      }, I don't think Aang was here`;
+    }
+  }
 
-const params = "waveHeight,airTemperature";
+  function formLogic(e) {
 
-const API_URL = "https://api.stormglass.io/v2/weather/point?";
-
-const Tomorrow_Io_Key = "zcrOLG4NfCDoP6VA1XBvDFzCqdUx6PZC";
-
-const Image_URL = "../../assets/images/";
-
-let arr = [];
-const arrLength = arr.length;
-
-const getWindSpeed = (e) => {
-  
-  e.preventDefault();
-  console.log(e.target.location.value)
-  const newLocation = e.target.location.value;
- 
-  axios
+    let arr = [];
+    const arrLength = arr.length;
+    
+    e.preventDefault();
+    const newLocation = e.target.location.value;
+    console.log(newLocation)
+    
+    axios
     .get(
       `https://api.tomorrow.io/v4/weather/realtime?location=${newLocation}&units=metric&apikey=zcrOLG4NfCDoP6VA1XBvDFzCqdUx6PZC`
     )
     .then((response) => {
-      console.log(response);
       console.log(response.data.data.values.windSpeed);
-      const windSpeedMs = response.data.data.values.windSpeed;
-      const windSpeedInMph = convertToMph(windSpeedMs);
+      const windSpeed = response.data.data.values.windSpeed;
 
-      console.log(windSpeedInMph);
-      const newWindSpeedValue = IsWindSpeedHigh(windSpeedInMph);
+      const windSpeedInMph = windSpeed* 2.237;
+      
+      const newWindSpeedValue = IsWindSpeedHigh(windSpeedInMph)
+
       console.log(newWindSpeedValue);
-      setCardValue(newWindSpeedValue)
+
+      // setCardValue(newWindSpeedValue)
+
+      return (newWindSpeedValue)
     })
     .catch((error) => {
       console.log(error);
       alert("please provide a valid location");
     });
-};
-
-function IsWindSpeedHigh(windSpeed) {
-  if (windSpeed > 10) {
-    return `Wind speed seems to be ${windSpeed + "mph"}, maybe Aang was here`;
-   
-  } else {
-    return `wind speed seems to be ${
-      windSpeed + "mph"
-    }, I don't think Aang was here`;
   }
-}
-
-
-
-function convertToMph(speed) {
-  const convertedWindSpeed = speed * 2.237;
-  console.log(convertedWindSpeed);
-  return convertedWindSpeed;
-}
-    return (
-      <div>
-        <form type="submit" class="app-form" id="form" onSubmit={getWindSpeed}>
-            <input
-            type="text"     
-            id="location"
-            class="app-form_input"
-            placeholder="location"
-            required
-            />
-            <button type="submit" class="app-form__button">
-                Discover
-            </button>
+    
+    // function IsWindSpeedHigh(windSpeed) {
+    //   if (windSpeed > 10) {
+    //     return `Wind speed seems to be ${windSpeed + "mph"}, maybe Aang was here`;
+    //   } else {
+    //     return `wind speed seems to be ${
+    //       windSpeed + "mph"
+    //     }, I don't think Aang was here`;
+    //   }
+    // }
+    // function convertToMph(speed) {
+    //   const convertedWindSpeed = speed * 2.237;
+    //   console.log(convertedWindSpeed);
+    //   return convertedWindSpeed;
+    // }
+  
+    
+  return (
+  <div>
+    <form type="submit" class="app-form" id="form" onSubmit={formLogic}>
+      <input
+      type="text"     
+      id="location"
+      class="app-form_input"
+      placeholder="location"
+      required
+      />
+      <button type="submit" class="app-form__button">
+        Discover</button>
         </form>  
-        <WindCard cardValue={cardValue}/>
-      </div>
-    )
+        {/* <WindCard cardValue = {{image: "../assets/images/aang-in-ice.jpg", windspeed: cardValue}} /> */}
+        <WindCard passedWindSpeedValue={this.props.newWindSpeedValue}/>
+    </div>
+  )
 }
 
+        
 export default Form;
--- src/components/wind-card/Wind-Card.jsx
+++ src/components/wind-card/Wind-Card.jsx
@@ -1,14 +1,20 @@
-import "./Wind-Card";
+import "./Wind-Card.scss"
+import aangInIce from "../../assets/images/aang-in-ice.jpg";
+import { useEffect, useState } from "react";
 
 const WindCard = ({cardValue}) => {
+   
+
+    console.log(cardValue)
+
     console.log(cardValue)
     return (
         <div className="app-card">
-                <div class="app-card_boundary">
-                    <p class="app-card_value" id="1">{cardValue}</p>
-                </div>
+                    {/* <img src={aangInIce} alt="aang in ice" /> */}
+            <p className="app-card_value" id="one">{cardValue}</p> 
         </div>
         )
+
     }
 
 export default WindCard;
