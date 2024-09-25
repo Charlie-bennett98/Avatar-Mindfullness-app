3e82d2bf8113300648a006f5644433a6f9b2a85c
Charlie-bennett98 <charleybennett698@gmail.com>
2024-09-25 13:35:10 +0100

    latest updates

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -6,42 +6,56 @@ import React from "react";
 
 const IsWindSpeedHigh = (windSpeed: number) => {
   if (windSpeed > 10) return `Wind speed seems to be ${windSpeed + "mph"}, maybe Aang was here`; 
-  
   return `wind speed seems to be ${windSpeed + "mph"}, I don't think Aang was here`;
 }
 
 const formLogic = async ({location}: {location: string}) => { 
+
+  const API_KEY = "fbaa5ed5b6fe4d1baa3103730242309&q"
+
+  console.log(location)
   const response = await axios.get(
-    `https://api.tomorrow.io/v4/weather/realtime?location=${location}&units=metric&apikey=zcrOLG4NfCDoP6VA1XBvDFzCqdUx6PZC`
+    //`https://api.tomorrow.io/v4/weather/realtime?location=${location}&units=metric&apikey=zcrOLG4NfCDoP6VA1XBvDFzCqdUx6PZC`
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}=${location}=no`
   )
-  
-  const windSpeed = response?.data?.data?.values?.windSpeed
+  console.log(response.data.current.wind_mph)
+  const windSpeed = response.data.current.wind_mph
+  // const windSpeed = response?.data?.data?.values?.windSpeed
 
   if (!windSpeed) return "No wind speed available"
 
-  const windSpeedInMph = windSpeed * 2.237
-  
-  const newWindSpeedValue = IsWindSpeedHigh(windSpeedInMph)
+  /* conversion to mph not needed for new api */
+  // const windSpeedInMph = windSpeed * 2.237
 
+  const newWindSpeedValue = IsWindSpeedHigh(windSpeed)
+  console.log(newWindSpeedValue)
   return newWindSpeedValue
+  
 }
 
 const Form = () => {
+
   const [windspeed, setWindspeed] = useState('')
   const [location, setLocation] = useState('')
+  const [saveButton, setSaveButton] = useState(false)
+
 
   const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
  
     const windSpeedValue = await formLogic({location})
+    setSaveButton(true)
 
     setWindspeed(windSpeedValue)
   }
+  console.log(windspeed)
 
   return (
     <div>
       <form className={styles.form} id="form" onSubmit={handleOnSubmit}>
-        <input
+        <div>
+          <input
           type="text"
           id="location"
           className={styles.input}
@@ -49,15 +63,21 @@ const Form = () => {
           required
           value={location}
           onChange={(e) => setLocation(e.target.value)}
-        />
-
-        <button type="submit" className={styles.button}>Discover</button>
-      </form>
-      
+          />
+          <button type="submit" className={styles.button}>Discover</button>
+        </div>
+        </form>
+      <div className ={styles.windcard_section}>
       {!!windspeed && <WindCard cardValue={windspeed}/>}
+      </div>
+      {saveButton && (
+      <button className="save-button">
+      save result
+    </button>)}
     </div>
   )
 }
 
         
 export default Form;
+
